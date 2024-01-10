4f150902d4c2d247c40ec1c11507120353ef81d4
Barth001 <alaekwechekwube@gmail.com>
2024-01-10 23:49:29 +0100

    database models

--- flightReservation/flight_app/models.py
+++ flightReservation/flight_app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Passenger(models.Model):
+    """Passenger database model"""
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=100)
+    mobile_number = models.CharField(max_length=11)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
+
+class Flight(models.Model):
+    """ Flight database model"""
+    operating_airline = models.CharField(max_length=225)
+    flight_number = models.CharField(max_length=225)
+    arrival_country = models.CharField(max_length=225)
+    departure_country = models.CharField(max_length=225)
+    operation_date = models.DateField()
+    time_departure = models.TimeField()
+
+    def __str__(self):
+        return self.operation_date + ' ' + self.flight_number
+
+
+class Reservation(models.Model):
+    passenger = models.OneToOneField(Flight, on_delete=models.CASCADE)
+    flight = models.OneToOneField(Passenger, on_delete=models.CASCADE)
